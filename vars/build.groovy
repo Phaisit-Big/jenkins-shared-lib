c647cbbd4c09b8040cb3c2d1649731459b6d195f
Phaisit-Big <Phaisit.Si@tnis.com>
2023-06-10 01:32:42 +0700

    init

---
+++ vars/build.groovy
@@ -0,0 +1,14 @@
+pipeline {
+    agent none
+    stages {
+        stage('Prepare') {
+            agent { label "node-docker-1"}
+            steps{
+                script{
+                    cleanWs()
+                    print "$WORKSPACE"
+                }
+            }
+        }
+    }
+}
